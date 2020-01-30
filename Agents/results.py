655b08df1309c0a4692e6209541aba63353dbc89
Tyler Malloy <tylerjmalloy@gmail.com>
2020-01-30 14:03:11 -0500

    updating resampling method

--- Agents/results.py
+++ Agents/results.py
@@ -7,101 +7,106 @@ plt.style.use('fivethirtyeight')
 sns.set(style="ticks", color_codes=True, rc={"lines.linewidth": 2.5})
 sns.set(font_scale=2.5)
 
-nchain_filename = "Pendulum"
+nchain_filenames = [    "Pendulum", # InvertedPendulumBulletEnv
+                        "InvertedPendulumSwingupBulletEnv",
+                        "InvertedDoublePendulumBulletEnv"
+                    ]
 
-NUM_AGENTS = 32
-NUM_RESAMPLES = 5
+NUM_RESAMPLES = 1
+NUM_RESAMPLES += 1 # 1 more than number of resamples
 # NUM_GENERALIZATION_EPISODES = 100 # Unused
 
-tags = [0.04] # 0.08
-tag_strings = []
-for tag in tags:
-    tag_strings.append(str(tag).replace(".", "p"))
+clac_tags = [[0.3], [], []] 
+clac_env_strings = []
+for env in clac_tags:
+    env_tags = []
+    for tag in env:
+        env_tags.append(str(tag).replace(".", "p"))
+    clac_env_strings.append(env_tags)
+
+sac_tags = [[0.3], [], []]
+sac_env_strings = []
+for env in sac_tags:
+    env_tags = []
+    for tag in env:
+        env_tags.append(str(tag).replace(".", "p"))
+    sac_env_strings.append(env_tags)
 
 All_Data = pd.DataFrame()
 
-agents = np.linspace(1, 32, 32, dtype="int")
+agents = np.linspace(1, 16, 16, dtype="int")
 
-for tag in tag_strings:
-    for agent_id in agents:
-        for resample_num in range(NUM_RESAMPLES):
+for index, nchain_filename in enumerate(nchain_filenames):
+    clac_tag_strings = clac_env_strings[index]
+    for tag in clac_tag_strings:
+        for agent_id in agents:
+            for resample_num in range(NUM_RESAMPLES):
 
-            clac_model_name = "CLAC" + "_" + str(tag) + "_" + str(agent_id) + "_" + str(resample_num) 
-            clac_model_file = nchain_filename + "/results/" + clac_model_name + ".pkl"
+                clac_model_name = "CLAC" + "_" + str(tag) + "_" + str(agent_id) + "_" + str(resample_num) 
+                clac_model_file = nchain_filename + "/results/" + clac_model_name + ".pkl"
 
-            sac_model_name = "SAC" + "_" + str(tag) + "_" + str(agent_id) + "_" + str(resample_num) 
-            sac_model_file = nchain_filename +  "/results/" + sac_model_name + ".pkl"
-            
-            #print(pd.read_pickle(sac_model_file))
+                clac_data = pd.read_pickle(clac_model_file)
+                clac_data["Resample"] = resample_num
+                clac_data["Environment"] = nchain_filename
+                clac_data["Timestep"] = clac_data["Timestep"].astype(float).round(-3)
+                clac_data["Mean Reward"] = clac_data["Episode Reward"].rolling(20).mean()
+                clac_data.loc[clac_data['Model'].str.contains('CLAC'), 'Model'] = 'CLAC'
 
-            clac_data = pd.read_pickle(clac_model_file)
-            sac_data = pd.read_pickle(sac_model_file)
+                All_Data = All_Data.append(clac_data, sort=False)
 
-            #clac_data["Coefficient"] = clac_data["mut_inf_coef"]
-            #sac_data["Coefficient"] = sac_data["ent_coef"]
+    sac_tag_strings = sac_env_strings[index]
+    for tag in sac_tag_strings:
+        for agent_id in agents:
+            for resample_num in range(NUM_RESAMPLES):
 
-            #clac_data = clac_data.iloc[-10:]
-            #sac_data = sac_data.iloc[-10:]
+                sac_model_name = "SAC" + "_" + str(tag) + "_" + str(agent_id) + "_" + str(resample_num) 
+                sac_model_file = nchain_filename +  "/results/" + sac_model_name + ".pkl"
+                sac_data = pd.read_pickle(sac_model_file)
+                sac_data["Resample"] = resample_num
+                sac_data["Environment"] = nchain_filename
+                sac_data["Timestep"] = sac_data["Timestep"].astype(float).round(-3)
+                sac_data["Mean Reward"] = sac_data["Episode Reward"].rolling(20).mean()
+                sac_data.loc[sac_data['Model'].str.contains('SAC'), 'Model'] = 'SAC'
 
-            clac_data.loc[clac_data['Model'].str.contains('CLAC'), 'Model'] = 'CLAC'
-            sac_data.loc[sac_data['Model'].str.contains('SAC'), 'Model'] = 'SAC'
+                All_Data = All_Data.append(sac_data, sort=False)
 
-            All_Data = All_Data.append(clac_data, sort=False)
-            All_Data = All_Data.append(sac_data, sort=False)
+#All_Data = All_Data.loc[All_Data["Resample"] == 1]
 
-print(All_Data)
+InvertedPendulumBulletEnv_Data = All_Data.loc[All_Data["Environment"] == nchain_filenames[0]]
+InvertedPendulumSwingupBulletEnv_Data = All_Data.loc[All_Data["Environment"] == nchain_filenames[1]]
+InvertedDoublePendulumBulletEnv_Data = All_Data.loc[All_Data["Environment"] == nchain_filenames[2]]
 
-ax = sns.lineplot(x="Resample", y="Episode Reward", hue="Model", data=All_Data, ci="sd")  
-plt.title('40K Time Step NChain Learning Results')
+fig, axes = plt.subplots(1, 3)
+
+InvertedPendulumBulletEnv_Data_0 = All_Data.loc[All_Data["Resample"] == 0]
+InvertedPendulumBulletEnv_Data_1 = All_Data.loc[All_Data["Resample"] == 1]
+
+ax0 = sns.lineplot(x="Timestep", y="Episode Reward", hue="Model", legend="full", ci="sd", data=InvertedPendulumBulletEnv_Data_0, ax=axes[0]) 
+ax0 = sns.lineplot(x="Timestep", y="Episode Reward", hue="Model", legend="full", ci="sd", data=InvertedPendulumBulletEnv_Data_1, ax=axes[1]) 
+
+
+#ax0 = sns.lineplot(x="Timestep", y="Episode Reward", hue="Model", legend="full", ci=68, data=InvertedPendulumBulletEnv_Data, ax=axes[0]) 
+#ax0 = sns.lineplot(x="Timestep", y="Episode Reward", hue="Model", legend="full", ci=68, data=InvertedPendulumSwingupBulletEnv_Data, ax=axes[1]) 
+#ax0 = sns.lineplot(x="Timestep", y="Episode Reward", hue="Model", legend="full", ci=68, data=InvertedDoublePendulumBulletEnv_Data, ax=axes[2]) 
+
+#ax0 = sns.lineplot(x="Resample", y="Episode Reward", hue="Model", legend="full", data=InvertedPendulumBulletEnv_Data, ax=axes[0]) 
+#ax0 = sns.lineplot(x="Resample", y="Episode Reward", hue="Model", legend="full", data=InvertedPendulumSwingupBulletEnv_Data, ax=axes[1]) 
+#ax0 = sns.lineplot(x="Resample", y="Episode Reward", hue="Model", legend="full", data=InvertedDoublePendulumBulletEnv_Data, ax=axes[2]) 
+
+#ax0 = sns.lineplot(x="Timestep", y="Episode Reward", hue="Coefficient", legend="full", data=CLAC_DATA, ax=axes[0]) 
+#ax1 = sns.lineplot(x="Timestep", y="Episode Reward", hue="Coefficient", legend="full", data=SAC_DATA, ax=axes[1]) 
+axes[0].set_title('Inverted Pendulum', fontsize=48)
+axes[1].set_title('SwingUp Pendulum', fontsize=48)
+axes[2].set_title('Double Pendulum', fontsize=48)
+
+axes[0].set_ylabel('Average Reward', fontsize=48)
+axes[1].set_ylabel('', fontsize=48)
+axes[2].set_ylabel('', fontsize=48)
+
+axes[0].set_xlabel('Resample Step', fontsize=48)
+axes[1].set_xlabel('Resample Step', fontsize=48)
+axes[2].set_xlabel('Resample Step', fontsize=48)
+
+fig.suptitle('Pendulum Generalization Results', fontsize=64)
+
 plt.show()
-
-assert(False)
-
-# df.loc[df['column_name'] == some_value]
-print(All_Data["Resample"].unique())
-Zero_Resample = All_Data.loc[All_Data["Resample"] == "0"]
-First_Resample = All_Data.loc[All_Data["Resample"] == "1"]
-Last_Resample = All_Data.loc[All_Data["Resample"] == "9"]
-
-Zero_Resample_CLAC = All_Data.loc[All_Data["Model"] == 'CLAC']
-Zero_Resample_CLAC = All_Data.loc[All_Data["Model"] == 'CLAC']
-
-Zero_Resample_SAC = All_Data.loc[All_Data["Model"] == 'SAC']
-Zero_Resample_SAC = All_Data.loc[All_Data["Model"] == 'SAC']
-
-print(Zero_Resample)
-
-g=sns.lineplot(x="Timestep", y="Episode Reward", hue="Model", data=Zero_Resample, ci="sd", alpha=0.1) 
-g.set(ylim=(-60, 0))
-
-plt.title('25K Time Step NChain Learning Results')
-plt.show()
-
-g=sns.lineplot(x="Timestep", y="Episode Reward", hue="Model", data=First_Resample, ci="sd", alpha=0.1) 
-g.set(ylim=(-60, 0))
-
-plt.title('25K Time Step NChain First Resample Results')
-plt.show()
-
-g=sns.lineplot(x="Timestep", y="Episode Reward", hue="Model", data=Last_Resample, ci="sd", alpha=0.1) 
-g.set(ylim=(-60, 0))
-plt.title('25K Time Step NChain Last Resample Results')
-plt.show()
-
-All_Data = pd.DataFrame()
-
-
-"""
-#All_Data = pd.read_pickle(nchain_filename + "/Generalization_Results_10K_5N.pkl")
-All_Data = pd.read_pickle(nchain_filename + "/Randomization_Results_10K_5N.pkl")
-
-print(All_Data)
-
-#g = sns.FacetGrid(All_Data, col="Model", sharex=False)
-#g.map(sns.boxplot, 'Coefficient', 'Reward')
-
-ax = sns.boxplot(x="Coefficient", y="Reward", hue="Model", data=All_Data)  # RUN PLOT   
-#ax.set_ylim(-150,-100)
-plt.title('250K Time Step NChain Randomization Results')
-plt.show()
-"""
--- examples/pybullet/gym/pybullet_envs/gym_pendulum_envs.py
+++ examples/pybullet/gym/pybullet_envs/gym_pendulum_envs.py
@@ -23,19 +23,33 @@ class InvertedPendulumBulletEnv(MJCFBaseBulletEnv):
 
   def reset(self):
     if (self.stateId >= 0):
-      #print("InvertedPendulumBulletEnv reset p.restoreState(",self.stateId,")")
       self._p.restoreState(self.stateId)
     r = MJCFBaseBulletEnv.reset(self)
     if (self.stateId < 0):
       self.stateId = self._p.saveState()
-      #print("InvertedPendulumBulletEnv reset self.stateId=",self.stateId)
+
+
+    # Reset dynamics after environment reset
+
+    self._p.setGravity(0, 0, -self.gravity)
+
+    bodyIndex = self.robot.parts["pole"].bodyIndex
+    bodyUniqueId = self.robot.parts["pole"].bodies[bodyIndex]
+    partIndex = self.robot.parts["pole"].bodyPartIndex
+    
+    self._p.changeDynamics(bodyUniqueId, partIndex, mass = self.poleMass)
+
     return r
 
   def get_features(self):
     return self.torqueForce, self.gravity, self.poleMass
   
-  def set_features(self, torqueForce=100, gravity=9.8, mass=5):
+  def set_features(self, features = [100, 9.8, 5]):
     self.reset()
+
+    torqueForce = features[0]
+    gravity = features[1]
+    mass = features[2]
     
     self.torqueForce = torqueForce
     self.gravity = gravity
@@ -46,12 +60,7 @@ class InvertedPendulumBulletEnv(MJCFBaseBulletEnv):
     bodyIndex = self.robot.parts["pole"].bodyIndex
     bodyUniqueId = self.robot.parts["pole"].bodies[bodyIndex]
     partIndex = self.robot.parts["pole"].bodyPartIndex
-
-    info = self._p.getDynamicsInfo(bodyUniqueId, partIndex)
     self._p.changeDynamics(bodyUniqueId, partIndex, mass = self.poleMass)
-
-    info = self._p.getDynamicsInfo(bodyUniqueId, partIndex)
-
 
   def randomize(self, level=0):
     self.reset()
@@ -62,20 +71,20 @@ class InvertedPendulumBulletEnv(MJCFBaseBulletEnv):
       self.poleMass = self.np_random.uniform(low=4.5, high=5.5)
 
     elif(level == 1):
-      if(self.np_random.uniform(low=0, high=0.5) > 0.5):
-        self.torqueForce = self.np_random.uniform(low=75, high=90)
+      if(self.np_random.uniform(low=0, high=1.0) > 0.5):
+        self.torqueForce = self.np_random.uniform(low=50, high=75)
       else:
-        self.torqueForce = self.np_random.uniform(low=110, high=125)
+        self.torqueForce = self.np_random.uniform(low=125, high=150)
       
-      if(self.np_random.uniform(low=0, high=0.5) > 0.5):
-        self.gravity = self.np_random.uniform(low=7.35, high=8.82)
+      if(self.np_random.uniform(low=0, high=1.0) > 0.5):
+        self.gravity = self.np_random.uniform(low=4.9, high=7.35)
       else:
-        self.gravity = self.np_random.uniform(low=10.78, high=12.25)
+        self.gravity = self.np_random.uniform(low=12.25, high=14.7)
 
-      if(self.np_random.uniform(low=0, high=0.5) > 0.5):
-        self.poleMass = self.np_random.uniform(low=3.75, high=4.5)
+      if(self.np_random.uniform(low=0, high=1.0) > 0.5):
+        self.poleMass = self.np_random.uniform(low=2.5, high=3.75)
       else:
-        self.poleMass = self.np_random.uniform(low=5.5, high=6.25)
+        self.poleMass = self.np_random.uniform(low=6.25, high=7.5)
 
     else:
       print("ERROR: Invalid randomization mode selected, only 0 or 1 are available")
@@ -88,20 +97,19 @@ class InvertedPendulumBulletEnv(MJCFBaseBulletEnv):
     bodyUniqueId = self.robot.parts["pole"].bodies[bodyIndex]
     partIndex = self.robot.parts["pole"].bodyPartIndex
 
-    info = self._p.getDynamicsInfo(bodyUniqueId, partIndex)
     self._p.changeDynamics(bodyUniqueId, partIndex, mass = self.poleMass)
-
-    info = self._p.getDynamicsInfo(bodyUniqueId, partIndex)
   
   def apply_action(self, a):
     assert (np.isfinite(a).all())
     if not np.isfinite(a).all():
       print("a is inf")
       a[0] = 0
-    self.slider.set_motor_torque(self.torqueForce * float(np.clip(a[0], -1, +1)))
+
+    self.robot.slider.set_motor_torque(self.torqueForce * float(np.clip(a[0], -1, +1)))
 
   def step(self, a):
-    self.robot.apply_action(a)
+    #self.robot.apply_action(a) # Need to have the torqueForce parameter to apply the correct action
+    self.apply_action(a)
     self.scene.global_step()
     state = self.robot.calc_state()  # sets self.pos_x self.pos_y
     vel_penalty = 0
@@ -120,12 +128,14 @@ class InvertedPendulumBulletEnv(MJCFBaseBulletEnv):
 
 
 class InvertedPendulumSwingupBulletEnv(InvertedPendulumBulletEnv):
-
   def __init__(self):
     self.robot = InvertedPendulumSwingup()
     MJCFBaseBulletEnv.__init__(self, self.robot)
     self.stateId = -1
 
+    self.torqueForce = 100
+    self.gravity = 9.8
+    self.poleMass = 5
 
 class InvertedDoublePendulumBulletEnv(MJCFBaseBulletEnv):
 
@@ -133,29 +143,127 @@ class InvertedDoublePendulumBulletEnv(MJCFBaseBulletEnv):
     self.robot = InvertedDoublePendulum()
     MJCFBaseBulletEnv.__init__(self, self.robot)
     self.stateId = -1
+
+    self.torqueForce = 200 # In double pendulum the initial torqueForce is 200
+    self.gravity = 9.8
+    self.pole1Mass = 5
+    self.pole2Mass = 5
   
+  def get_features(self):
+    return self.torqueForce, self.gravity, self.pole1Mass, self.pole2Mass
+  
+  def set_features(self, features = [200, 9.8, 5, 5]):
+    self.reset()
+    
+    torqueForce = features[0]
+    gravity = features[1]
+    pole1Mass = features[2] 
+    pole2Mass = features[3]
+
+    self.torqueForce = torqueForce
+    self.gravity = gravity
+    self.pole1Mass = pole1Mass
+    self.pole2Mass = pole2Mass
+
+    self._p.setGravity(0, 0, -self.gravity)
+
+    bodyIndex = self.robot.parts["pole"].bodyIndex
+    bodyUniqueId = self.robot.parts["pole"].bodies[bodyIndex]
+    partIndex = self.robot.parts["pole"].bodyPartIndex
+    self._p.changeDynamics(bodyUniqueId, partIndex, mass = self.pole1Mass)
+
+    bodyIndex = self.robot.parts["pole2"].bodyIndex
+    bodyUniqueId = self.robot.parts["pole2"].bodies[bodyIndex]
+    partIndex = self.robot.parts["pole2"].bodyPartIndex
+    self._p.changeDynamics(bodyUniqueId, partIndex, mass = self.pole2Mass)
+
   def randomize(self, level=0):
-    print("randomizing")
+    self.reset()
+
+    if(level == 0):
+      self.torqueForce = self.np_random.uniform(low=180, high=220)
+      self.gravity = self.np_random.uniform(low=8.82, high=10.78)
+      self.pole1Mass = self.np_random.uniform(low=4.5, high=5.5)
+      self.pole2Mass = self.np_random.uniform(low=4.5, high=5.5)
+    elif(level == 1):
+      if(self.np_random.uniform(low=0, high=1.0) > 0.5):
+        self.torqueForce = self.np_random.uniform(low=50, high=75)
+      else:
+        self.torqueForce = self.np_random.uniform(low=125, high=150)
+      
+      if(self.np_random.uniform(low=0, high=1.0) > 0.5):
+        self.gravity = self.np_random.uniform(low=4.9, high=7.35)
+      else:
+        self.gravity = self.np_random.uniform(low=12.25, high=14.7)
+
+      if(self.np_random.uniform(low=0, high=1.0) > 0.5):
+        self.pole1Mass = self.np_random.uniform(low=2.5, high=3.75)
+      else:
+        self.pole1Mass = self.np_random.uniform(low=6.25, high=7.5)
+      
+      if(self.np_random.uniform(low=0, high=1.0) > 0.5):
+        self.pole2Mass = self.np_random.uniform(low=2.5, high=3.75)
+      else:
+        self.pole2Mass = self.np_random.uniform(low=6.25, high=7.5)
+
+    else:
+      print("ERROR: Invalid randomization mode selected, only 0 or 1 are available")
+    
+    #print("randomizing, torque: ", self.torqueForce  , " gravity ", self.gravity)
+
+    self._p.setGravity(0, 0, -self.gravity)
+
+    bodyIndex = self.robot.parts["pole"].bodyIndex
+    bodyUniqueId = self.robot.parts["pole"].bodies[bodyIndex]
+    partIndex = self.robot.parts["pole"].bodyPartIndex
+
+    self._p.changeDynamics(bodyUniqueId, partIndex, mass = self.pole1Mass)
+
+    bodyIndex = self.robot.parts["pole2"].bodyIndex
+    bodyUniqueId = self.robot.parts["pole2"].bodies[bodyIndex]
+    partIndex = self.robot.parts["pole2"].bodyPartIndex
+
+    self._p.changeDynamics(bodyUniqueId, partIndex, mass = self.pole2Mass)
 
   def create_single_player_scene(self, bullet_client):
     return SingleRobotEmptyScene(bullet_client, gravity=9.8, timestep=0.0165, frame_skip=1)
 
   def reset(self):
+
     if (self.stateId >= 0):
       self._p.restoreState(self.stateId)
     r = MJCFBaseBulletEnv.reset(self)
     if (self.stateId < 0):
       self.stateId = self._p.saveState()
+
+    self._p.setGravity(0, 0, -self.gravity)
+
+    bodyIndex = self.robot.parts["pole"].bodyIndex
+    bodyUniqueId = self.robot.parts["pole"].bodies[bodyIndex]
+    partIndex = self.robot.parts["pole"].bodyPartIndex
+    self._p.changeDynamics(bodyUniqueId, partIndex, mass = self.pole1Mass)
+
+    bodyIndex = self.robot.parts["pole2"].bodyIndex
+    bodyUniqueId = self.robot.parts["pole2"].bodies[bodyIndex]
+    partIndex = self.robot.parts["pole2"].bodyPartIndex
+    self._p.changeDynamics(bodyUniqueId, partIndex, mass = self.pole2Mass)
+    
     return r
+  
+  def apply_action(self, a):
+    assert (np.isfinite(a).all())
+    self.robot.slider.set_motor_torque(self.torqueForce * float(np.clip(a[0], -1, +1)))
 
   def step(self, a):
-    self.robot.apply_action(a)
+    #print("GLOBAL: torqueForce: ", self.torqueForce, " gravity: ", self.gravity, " pole1Mass: ", self.pole1Mass, " pole2Mass: ", self.pole2Mass)
+    #self.robot.apply_action(a)
+    self.apply_action(a)
     self.scene.global_step()
     state = self.robot.calc_state()  # sets self.pos_x self.pos_y
     # upright position: 0.6 (one pole) + 0.6 (second pole) * 0.5 (middle of second pole) = 0.9
     # using <site> tag in original xml, upright position is 0.6 + 0.6 = 1.2, difference +0.3
     dist_penalty = 0.01 * self.robot.pos_x**2 + (self.robot.pos_y + 0.3 - 2)**2
-    # v1, v2 = self.model.data.qvel[1:3]   TODO when this fixed https://github.com/bulletphysics/bullet3/issues/1040
+    # v1, v2 = self.model.data.qvel[1:3] TODO when this fixed https://github.com/bulletphysics/bullet3/issues/1040
     #vel_penalty = 1e-3 * v1**2 + 5e-3 * v2**2
     vel_penalty = 0
     alive_bonus = 10
